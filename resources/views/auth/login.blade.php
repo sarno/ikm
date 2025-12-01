8611b50209223fbd7b2219d4d97725adecabf2e2
sarno <sarno.okre@gmail.com>
2025-12-01 12:42:14 +0700

    bugfix devide by zero

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -60,12 +60,12 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has("password.request"))
-                    <a
+                    <!-- <a
                         class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         href="{{ route("password.request") }}"
                     >
                         {{ __("Forgot your password?") }}
-                    </a>
+                    </a> -->
                 @endif
 
                 <x-button class="ms-4">
